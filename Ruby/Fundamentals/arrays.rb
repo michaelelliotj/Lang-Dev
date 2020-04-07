d2551cb1af6e5b5d8ad629ef5d90ef4127444510
Michael E Jones <michaelelliotj@gmail.com>
2020-04-07 03:40:14 +0000

    Structure update/git test

---
+++ Ruby/Fundamentals/arrays.rb
@@ -0,0 +1,11 @@
+ary = Array.new() ##
+ary = Array[]     ## All the same thing
+ary = []          ##
+
+
+numAry    = Array.new(0..10)
+fooAry    = Array.new(5, 'bar')
+doubleAry = Array.new(5) {|x| x = x * 2}
+alphAry   = Array.new('a'..'z')
+
+
